55e2c65cc477e21695a306befb1854eb3e20957a
jaysonwcs <jaysonwcs@gmail.com>
2021-10-18 10:43:36 -0300

    Corrected Bluetooth driver installation script

--- Drivers/Bluetooth.sh
+++ Drivers/Bluetooth.sh
@@ -3,6 +3,7 @@ read -p "Pressione [Enter] para continuar:"
 
 mkdir Downloads
 cd Downloads/
+CWD=$(pwd)
 read -p "Pressione [Enter] para continuar:"
 wget https://mpow.s3-us-west-1.amazonaws.com/mpow_BH519A_driver+for+Linux.7z
 read -p "Pressione [Enter] para continuar:"
@@ -13,9 +14,10 @@ cd 202*/rtkbt-firmware/$D/rtlbt
 S=sudo;D=$D/rtl_bt/$N
 for C in a_ b_;do for L in fw $G;do F=$D$C$L$E;[ -f $F ]&&$S cp $F{,-`date +%s`};$S cp -f $N$C$L $F;done;done
 
-cd ..
+cd "${CWD}"
 read -p "Pressione [Enter] para continuar:"
 
-rm -r 20201202_LINUX_BT_DRIVER/
+rm -r 20201202_LINUX_BT_DRIVER
+rm mpow_BH519A_driver+for+Linux.7z
 
 echo
