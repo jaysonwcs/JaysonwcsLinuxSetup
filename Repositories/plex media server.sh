cc32f26456cfae49ec1ecb4b54fcfd505960174a
jaysonwcs <jaysonwcs@gmail.com>
2021-10-18 10:46:09 -0300

    Plex repository bug corrected

--- Repositories/plex media server.sh
+++ Repositories/plex media server.sh
@@ -2,7 +2,7 @@ echo -e "${BLUE}Adicionando repositório Plex Media Server${RESET}"
 read -p "Pressione [Enter] para continuar:"
 sudo touch /etc/apt/sources.list.d/plexmediaserver.list
 read -p "Pressione [Enter] para continuar:"
-echo "deb https://downloads.plex.tv/repo/deb/ public main" | sudo tee /etc/apt/sources.list.d/plexmediaserver.listdeb https://downloads.plex.tv/repo/deb/ public main
+echo "deb https://downloads.plex.tv/repo/deb/ public main" | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 read -p "Pressione [Enter] para continuar:"
 wget -q https://downloads.plex.tv/plex-keys/PlexSign.key -O - | sudo apt-key add -
 read -p "Pressione [Enter] para continuar:"
