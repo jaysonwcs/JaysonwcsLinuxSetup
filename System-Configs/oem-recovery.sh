8f4ebc5a609714c24c6627cf6752476e5aa9462b
jaysonwcs <jaysonwcs@gmail.com>
2021-10-18 11:29:20 -0300

    Fixed oem-recovery setup

--- System-Configs/oem-recovery.sh
+++ System-Configs/oem-recovery.sh
@@ -2,6 +2,7 @@ echo -e "${BLUE}${BOLD}Configurando OEM Install:"
 echo -e "${BLUE}${BOLD}=========================${RESET}"
 read -p "Pressione [Enter] para continuar:"
 echo -e "${BLUE}Baixando ISO Kubuntu${RESET}"
+mkdir Downloads
 cd Downloads/
 wget "https://cdimage.ubuntu.com/kubuntu/releases/20.04.3/release/kubuntu-20.04.3-desktop-amd64.iso"
 read -p "Pressione [Enter] para continuar:"
