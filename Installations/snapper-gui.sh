1735c08d1b5da18c3d97d712aab98edab3d95e51
jaysonwcs <jaysonwcs@gmail.com>
2021-10-18 11:24:07 -0300

    Fixed snapper-gui install script

--- Installations/snapper-gui.sh
+++ Installations/snapper-gui.sh
@@ -1,12 +1,14 @@
 echo -e "${BLUE}Instalando Snapper GUI${RESET}"
 read -p "Pressione [Enter] para continuar:"
+mkdir Sources
 cd Sources/
 git clone https://github.com/ricardomv/snapper-gui.git
 cd snapper-gui/
 read -p "Pressione [Enter] para continuar:"
-python3 setup.py install
+sudo python3 setup.py install
 cd ..
 read -p "Pressione [Enter] para continuar:"
-rm -rf snapper-gui/
+sudo rm -rf snapper-gui/
 read -p "Pressione [Enter] para continuar:"
+cd ..
 echo
