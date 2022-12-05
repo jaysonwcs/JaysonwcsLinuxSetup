a05d5f9785cd0bc28e735e91206c14bde2e010dd
Jayson <jaysonwcs@gmail.com>
2022-12-05 11:50:21 -0300

    Fixed some errors

--- create-subvols-and-mount.sh
+++ create-subvols-and-mount.sh
@@ -16,6 +16,7 @@ mkdir var
 btrfs subvolume create var/@flatpak
 btrfs subvolume create var/@snap
 btrfs subvolume create var/@cache
+btrfs subvolume create var/@tmp
 btrfs subvolume create var/@main
 btrfs subvolume create @root_user
 mkdir btrbk_snapshots
@@ -27,12 +28,13 @@ mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/@ /mnt
 cd /mnt
 
 mkdir {btr_pool,boot,home,root,var}
+mkdir var/{cache,tmp,lib,snap}
 mount /dev/sda1 boot
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/var/@main var
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/@home home
-mkdir var/cache
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/var/@cache var/cache
-mkdir -p var/lib/flatpak
+mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/var/@tmp var/tmp
+mkdir var/lib/flatpak
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/var/@flatpak var/lib/flatpak
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/var/@snap var/snap
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/ btr_pool
