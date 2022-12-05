b30a6f680380ecbc68d0244efc88ae0355e8d4dd
Jayson <jaysonwcs@gmail.com>
2022-12-05 19:48:09 -0300

    Fixed some errors

--- configure-system-step2.sh
+++ configure-system-step2.sh
@@ -20,7 +20,7 @@ systemctl enable cockpit.socket
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/marlonrichert/zsh-hist.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-hist
 
-sed -i 's/plugins=(git)/plugins=(git z zsh-autosuggestions safe-paste sudo themes zsh-hist)/g' .zshrc
+sed -i 's/plugins=(git)/plugins=(git z zsh-autosuggestions safe-paste sudo themes zsh-hist)/g' ~/.zshrc
 
 cd /
 mkdir Sources
