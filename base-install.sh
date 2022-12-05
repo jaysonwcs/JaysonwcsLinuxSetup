df6f6c7e1cae9cc0aac943f6b81cfcb0e06bab7c
Jayson <jaysonwcs@gmail.com>
2022-12-05 14:44:54 -0300

    Fixed some errors

--- base-install.sh
+++ base-install.sh
@@ -5,4 +5,4 @@ sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
 sed -i 's/#[multilib]/[multilib]/g' pacman.conf /etc/pacman.conf
 sed -i 's/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/g' /etc/pacman.conf
 
-pacstrap /mnt base base-devel linux linux-headers linux-firmware linux-lts linus-lts-headers nano btrfs-progs kitty-terminfo git zsh
+pacstrap /mnt base base-devel linux linux-headers linux-firmware linux-lts linux-lts-headers nano btrfs-progs kitty-terminfo git zsh
