69f3ed5659b19b4eff7139034245243f1239c583
Jayson <jaysonwcs@gmail.com>
2022-12-06 13:06:02 -0300

    Fixed some errors

---
+++ install-gnome-extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/Sources
+git clone https://github.com/cyfrost/install-gnome-extensions
+cd install-gnome-extensions
+./install-gnome-extensions.sh --enable --file ~/Sources/JaysonwcsLinuxSetup/gnome-extensions
