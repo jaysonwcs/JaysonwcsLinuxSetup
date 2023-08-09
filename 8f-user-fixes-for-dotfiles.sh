c36368ef5999239e949ab4bb3c10b1fab23cb723
Jayson <jaysonwcs@gmail.com>
2023-08-09 16:51:39 -0300

    Some fixes

---
+++ 8f-user-fixes-for-dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/.local/share
+mv ~/.oh-my-zsh ~/.local/share/oh-my-zsh
+
+mkdir -p ~/.local/state/zsh
+mv ~/.zsh_history ~/.local/state/zsh/history
+
+mkdir -p ~/.config/zsh
+mv ~/.zshrc ~/.config/zsh/.zshrc
