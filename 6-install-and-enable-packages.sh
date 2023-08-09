a19f01d690ee9ee55f25730854c69c2210be6225
Jayson <jaysonwcs@gmail.com>
2023-08-09 14:37:48 -0300

    Created script to fiz dotfolders on home

--- 6-install-and-enable-packages.sh
+++ 6-install-and-enable-packages.sh
@@ -8,9 +8,9 @@ reflector --country Brazil --sort rate --latest 15 --save /etc/pacman.d/mirrorli
 
 pacman -S - < packages_pacman
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot/efi --bootloader-id=GRUB
 
-grub-mkconfig -o /boot/grub/grub.cfg
+grub-mkconfig -o /boot/efi/grub/grub.cfg
 
 #sudo efibootmgr -d /dev/sda1 -c -L "Grub" -l '\EFI\SYSTEM\grubx64.efi'
 
