23966c30c895cb4c435b53fda9433aa8181510c1
Jayson <jaysonwcs@gmail.com>
2023-09-21 13:12:48 -0300

    install recovery script changed to use ISO file instead of loop device

--- 1c-install-recovery.sh
+++ 1c-install-recovery.sh
@@ -2,20 +2,23 @@
 
 if [ -z "$1" ] || [ -z "$2" ] // [ -z "$3" ]
   then
-    echo "Este script deve ser chamado passando o disco (dispositivo) da ISO do Arch Linux, partição onde será instalado e partição EFI: comando [DEV-ARCH] [PART-DESTINO] [PART-EFI]"
+    echo "Este script deve ser chamado passando o arquivo da ISO do Arch Linux, partição onde será instalado e partição EFI: comando [DEV-ARCH] [PART-DESTINO] [PART-EFI]"
+    echo "Exemplo: ./1c-install-recovery.sh arch.iso /dev/sda2 /dev/sda1"
 
 else
-	echo "Dev Arch: $1 , part recovery: $2, part EFI: $3"
+	echo "Arquivo ISO: $1 , part recovery: $2, part EFI: $3"
 
-	echo "Comando:"
+	#echo "Comando:"
 	# echo "sudo efibootmgr -d /dev/sda1 -c -L \"Arch EFI\" -l '\vmlinuz-linux' -u \"root=UUID=$1 rw sysrq_always_enabled=1 resume=UUID=$2 rootflags=subvol=@ initrd=initramfs-linux.img\""
 	
-	dd bs=4M if=${1}1 of=$2 conv=fsync oflag=direct status=progress
+	echo "Gravando arquivo ISO $1 na partição $2"
+	dd bs=4M if=${1} of=$2 conv=fsync oflag=direct status=progress
 
 	mkdir /mnt/{arch-boot,system-boot}
-	mount ${1}2 /mnt/arch-boot
+	LOOPDEVICE=$(losetup -fP --show $1) 
+	mount ${LOOPDEVICE}2 /mnt/arch-boot
 	mount $3 /mnt/system-boot
-	rsync -ahPv /mnt/arch-boot/ /mnt/system-boot
+	rsync -ahPvv /mnt/arch-boot/ /mnt/system-boot
 
 	umount /mnt/arch-boot
 	umount /mnt/system-boot
