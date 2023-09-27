dcb5770e400cc6ea15eba68c8d71503ecf608d80
Jayson <jaysonwcs@gmail.com>
2023-09-27 14:14:33 -0300

    Cleaned script 2 and fixed some subvolumes

--- 2-create-subvols-and-mount.sh
+++ 2-create-subvols-and-mount.sh
@@ -15,6 +15,11 @@ btrfs subvolume create var/@snap
 btrfs subvolume create var/@main
 btrfs subvolume create var/@plex
 btrfs subvolume create @root_user
+btrfs subvolume create @games
+btrfs subvolume create @steam
+btrfs subvolume create @flatpak_home
+btrfs subvolume create @flatpak
+btrfs subvolume create @snap
 mkdir btrbk_snapshots
 
 cd -
@@ -34,39 +39,29 @@ mkdir -p home/jayson/{.cache,.var,.local,Downloads,Games}
 mkdir home/jayson/.var/app
 mkdir -p home/jayson/.local/share/Steam
 
-mkdir btr_pool/@bkp_off/{games,downloads,cache_home,flatpak_home,steam}
+mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=@games home/jayson/Games
+mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=@steam home/jayson/.local/share/Steam
+mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=@flatpak_home home/jayson/.var/app
 
-mount --bind btr_pool/@bkp_off/games home/jayson/Games
+mkdir btr_pool/@bkp_off/{downloads,cache_home}
+mkdir btr_pool/@bkp_off/{cache,tmp,downloads_root,cache_root}
+
 mount --bind btr_pool/@bkp_off/downloads home/jayson/Downloads
 mount --bind btr_pool/@bkp_off/cache_home home/jayson/.cache
-mount --bind btr_pool/@bkp_off/flatpak_home home/jayson/.var/app
-mount --bind btr_pool/@bkp_off/steam home/jayson/.local/share/Steam
 
 mkdir var/{cache,tmp,lib,snap}
 mkdir var/lib/flatpak
 
-mkdir btr_pool/@bkp_off/{cache,tmp,flatpak,snap,downloads_root,cache_root}
+mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=@snap var/snap
+mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=@flatpak var/lib/flatpak
 
 mount --bind btr_pool/@bkp_off/cache var/cache
 mount --bind btr_pool/@bkp_off/tmp var/tmp
-mount --bind btr_pool/@bkp_off/flatpak var/lib/flatpak
-mount --bind btr_pool/@bkp_off/snap var/snap
-
-# mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=/var/@cache var/cache
-# mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=/var/@tmp var/tmp
-
-# mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=/var/@flatpak var/lib/flatpak
-# mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=/var/@snap var/snap
 
 mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=@root_user root
 mkdir root/{.cache,Downloads}
 
 mount --bind btr_pool/@bkp_off/downloads_root root/Downloads
 mount --bind btr_pool/@bkp_off/cache_root root/.cache
-
-# mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=/users/root/@downloads root/Downloads
-# mount /dev/sda3 -o noatime,compress=zstd,commit=120,subvol=/users/root/@cache root/.cache
-
-# swapon /dev/sda3
 
 cd ~
