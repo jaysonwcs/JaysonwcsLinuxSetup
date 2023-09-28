20b05590e921895d1e3bcbbf52c0b9c05bdc192c
Jayson <jaysonwcs@gmail.com>
2023-09-28 10:51:41 -0300

    Fixed error on script 2b

--- 2b-mount.sh
+++ 2b-mount.sh
@@ -11,7 +11,7 @@ if [ ! -d "btr_pool" ]; then
 	CREATE_DIRS = true
 fi
 
-if [ $CREATE_DIRS = true ]
+if [ "$CREATE_DIRS" = true ]
 	mkdir {btr_pool,boot,home,root,var}
 fi
 
@@ -20,7 +20,7 @@ mount $1 boot
 mount $2 -o noatime,compress=zstd,commit=120,subvol=/var/@main var
 mount $2 -o noatime,compress=zstd,commit=120,subvol=@home home
 
-if [ $CREATE_DIRS = true ]
+if [ "$"CREATE_DIRS" = true ]
 	mkdir -p home/jayson/{.cache,.var,.local,Downloads,Games}
 	mkdir home/jayson/.var/app
 	mkdir -p home/jayson/.local/share/Steam
@@ -30,7 +30,7 @@ mount $2 -o noatime,compress=zstd,commit=120,subvol=@games home/jayson/Games
 mount $2 -o noatime,compress=zstd,commit=120,subvol=@steam home/jayson/.local/share/Steam
 mount $2 -o noatime,compress=zstd,commit=120,subvol=@flatpak_home home/jayson/.var/app
 
-if if [ $CREATE_DIRS = true ]
+if if [ "$CREATE_DIRS" = true ]
 	mkdir btr_pool/@bkp_off/{downloads,cache_home}
 	mkdir btr_pool/@bkp_off/{cache,tmp,downloads_root,cache_root}
 fi
@@ -38,7 +38,7 @@ fi
 mount --bind btr_pool/@bkp_off/downloads home/jayson/Downloads
 mount --bind btr_pool/@bkp_off/cache_home home/jayson/.cache
 
-if [ $CREATE_DIRS = true ]if
+if [ "$CREATE_DIRS" = true ]if
 	mkdir var/{cache,tmp,lib,snap}
 	mkdir var/lib/flatpak
 fi
@@ -51,7 +51,7 @@ mount --bind btr_pool/@bkp_off/tmp var/tmp
 
 mount $2 -o noatime,compress=zstd,commit=120,subvol=@root_user root
 
-if [ $CREATE_DIRS = true ]
+if [ "$CREATE_DIRS" = true ]
 	mkdir root/{.cache,Downloads}
 fi
 
