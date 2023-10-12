8d18d6ce167e07d7f46c9232d4eafe7b9ad5719d
Jayson <jaysonwcs@gmail.com>
2023-10-11 21:04:33 -0300

    install-grub script created

--- arch-linux-installation/install-rescue-system/install-grub.sh
+++ arch-linux-installation/install-rescue-system/install-grub.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo arch-chroot "$1"
+arch-chroot $1 /bin/bash -c "su - -c 'cd ~/; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB'"
+
+arch-chroot $1 /bin/bash -c "su - -c 'cd ~/; grub-mkconfig -o /boot/grub/grub.cfg'"
