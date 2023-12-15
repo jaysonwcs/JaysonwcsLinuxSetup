fcc5d6510eacb89a35ff27d663b5dab57a660964
Jayson <jaysonwcs@gmail.com>
2023-12-15 17:22:46 -0300

    fixed script that creates partitions skipping boot

--- arch-linux-installation/1a-partition-skip-boot.sh
+++ arch-linux-installation/1a-partition-skip-boot.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 
-# bash x.sh /dev/sda 5 6
-
-end_second_partition=$(($2 + $3))
-
-echo "First: size $2 GiB, End: $2 GiB"
-echo "Second: size $3 GiB, End: $end_second_partition GiB"
+# bash x.sh /dev/sda 6
 
 END_BOOT_PART=$(sudo parted -sa optimal -- "$1" print | grep boot | awk '{print $3}')
+
+# end_second_partition=$(($2 + $3))
+SIZE_IN_MB=$(($2 + 1024))
+END_SECOND_PARTITION=$(($END_BOOT_PART + $SIZE_IN_MB))
+
+# echo "First: size $2 GiB, End: $2 GiB"
+echo "Second: size $2 GiB, End: $END_SECOND_PARTITION MB"
 
 sudo parted -sa optimal -- "$1" \
 # mklabel gpt \
 # mkpart boot fat32 0% "$2"GiB set 1 boot on \
-mkpart swap linux-swap "$END_BOOT_PART" "$end_second_partition"GiB set 2 swap on \
-mkpart system btrfs "$end_second_partition"GiB -1 \
+mkpart swap linux-swap "$END_BOOT_PART" "$END_SECOND_PARTITION"MB set 2 swap on \
+mkpart system btrfs "$END_SECOND_PARTITION"MB -1 \
 print
