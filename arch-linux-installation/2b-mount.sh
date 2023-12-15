48710913988aa1df2acfcbf8616512c572943d49
Jayson <jaysonwcs@gmail.com>
2023-12-15 17:50:15 -0300

    script for subvolume mounts improved

--- arch-linux-installation/2b-mount.sh
+++ arch-linux-installation/2b-mount.sh
@@ -6,7 +6,7 @@
 
 SSD_options="noatime,compress=zstd,commit=120"
 
-mount "$2" -o $SSD_options,subvol=/@ $3
+mount -v "$2" -o $SSD_options,subvol=/@ $3
 cd "$3" || exit
 
 if [ ! -d "btr_pool" ]; then
@@ -14,50 +14,50 @@ if [ ! -d "btr_pool" ]; then
 fi
 
 if [ "$CREATE_DIRS" = true ]; then
-	mkdir {btr_pool,boot,home,root,var}
+	mkdir -v {btr_pool,boot,home,root,var}
 fi
 
-mount "$2" -o $SSD_options,subvol=/ btr_pool
-mount "$1" boot
-mount "$2" -o $SSD_options,subvol=/var/@main var
-mount "$2" -o $SSD_options,subvol=@home home
+mount -v "$2" -o $SSD_options,subvol=/ btr_pool
+mount -v "$1" boot
+mount -v "$2" -o $SSD_options,subvol=/var/@main var
+mount -v "$2" -o $SSD_options,subvol=@home home
 
 if [ "$CREATE_DIRS" = true ]; then
-	mkdir -p home/jayson/{.cache,.var,.local,Downloads,Games}
-	mkdir home/jayson/.var/app
-	mkdir -p home/jayson/.local/share/Steam
+	mkdir -v -p home/jayson/{.cache,.var,.local,Downloads,Games}
+	mkdir -v home/jayson/.var/app
+	mkdir -v -p home/jayson/.local/share/Steam
 fi
 
-mount "$2" -o $SSD_options,subvol=@games home/jayson/Games
-mount "$2" -o $SSD_options,subvol=@steam home/jayson/.local/share/Steam
-mount "$2" -o $SSD_options,subvol=@flatpak_home home/jayson/.var/app
+mount -v "$2" -o $SSD_options,subvol=@games home/jayson/Games
+mount -v "$2" -o $SSD_options,subvol=@steam home/jayson/.local/share/Steam
+mount -v "$2" -o $SSD_options,subvol=@flatpak_home home/jayson/.var/app
 
 if [ "$CREATE_DIRS" = true ]; then
-	mkdir btr_pool/@bkp_off/{downloads,cache_home}
-	mkdir btr_pool/@bkp_off/{cache,tmp,downloads_root,cache_root}
+	mkdir -v btr_pool/@bkp_off/{downloads,cache_home}
+	mkdir -v btr_pool/@bkp_off/{cache,tmp,downloads_root,cache_root}
 fi
 
-mount --bind btr_pool/@bkp_off/downloads home/jayson/Downloads
-mount --bind btr_pool/@bkp_off/cache_home home/jayson/.cache
+mount -v --bind btr_pool/@bkp_off/downloads home/jayson/Downloads
+mount -v --bind btr_pool/@bkp_off/cache_home home/jayson/.cache
 
 if [ "$CREATE_DIRS" = true ]; then
-	mkdir var/{cache,tmp,lib,snap}
-	mkdir var/lib/flatpak
+	mkdir -v var/{cache,tmp,lib,snap}
+	mkdir -v var/lib/flatpak
 fi
 
-mount "$2" -o $SSD_options,subvol=@snap var/snap
-mount "$2" -o $SSD_options,subvol=@flatpak var/lib/flatpak
+mount -v "$2" -o $SSD_options,subvol=@snap var/snap
+mount -v "$2" -o $SSD_options,subvol=@flatpak var/lib/flatpak
 
-mount --bind btr_pool/@bkp_off/cache var/cache
-mount --bind btr_pool/@bkp_off/tmp var/tmp
+mount -v --bind btr_pool/@bkp_off/cache var/cache
+mount -v --bind btr_pool/@bkp_off/tmp var/tmp
 
-mount "$2" -o $SSD_options,subvol=@root_user root
+# mount -v "$2" -o $SSD_options,subvol=@root_user root
 
 if [ "$CREATE_DIRS" = true ]; then
-	mkdir root/{.cache,Downloads}
+	mkdir -v root/{.cache,Downloads}
 fi
 
-mount --bind btr_pool/@bkp_off/downloads_root root/Downloads
-mount --bind btr_pool/@bkp_off/cache_root root/.cache
+mount -v --bind btr_pool/@bkp_off/downloads_root root/Downloads
+mount -v --bind btr_pool/@bkp_off/cache_root root/.cache
 
 cd ~ || exit
