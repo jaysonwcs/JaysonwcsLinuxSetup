bdf8f7cea1ac76ff64c26c606d5b1d41cf427655
Jayson <jaysonwcs@gmail.com>
2023-12-15 17:39:08 -0300

    script for subvolume creation improved

--- arch-linux-installation/2a-create-subvols.sh
+++ arch-linux-installation/2a-create-subvols.sh
@@ -3,29 +3,29 @@
 # $1 = system partition, $2 = mounted folder
 #example:
 #./2-create-subvols-and-mount.sh /dev/sda3 /mnt
-mount $1 $2
+mount -v $1 $2
 cd $2
 
 btrfs subvolume create @
 btrfs subvolume create @home
 btrfs subvolume create @etc
-mkdir @etc/work
-mkdir @etc/upper
+mkdir -v @etc/work
+mkdir -v @etc/upper
 btrfs subvolume create @bkp_off
-mkdir -p var/_btrbk_snap
+mkdir -v -p var/_btrbk_snap
 btrfs subvolume create var/@flatpak
 btrfs subvolume create var/@snap
 btrfs subvolume create var/@main
 btrfs subvolume create var/@plex
-btrfs subvolume create @root_user
+# btrfs subvolume create @root_user
 btrfs subvolume create @games
 btrfs subvolume create @steam
 btrfs subvolume create @flatpak_home
 btrfs subvolume create @flatpak
 btrfs subvolume create @snap
-mkdir btrbk_snapshots
+mkdir -v btrbk_snapshots
 
 tree
 
 cd -
-umount $2
+umount -v $2
