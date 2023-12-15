a8227824cab527e61c336348f802cb595dd3424a
Jayson <jaysonwcs@gmail.com>
2023-12-15 16:59:35 -0300

    fixed immutable system script

--- immutable-system/enter-update-system.sh
+++ immutable-system/enter-update-system.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-btrfs subvolume delete /btr_pool/@.old
-SNAPSHOT=@upd.$(date +"%Y%m%dT%H%M%S")
-btrfs subvolume snapshot -r /btr_pool/@ /btr_pool/update_snapshots/${SNAPSHOT}
-mv /btr_pool/@ /btr_pool/@.old
-btrfs subvolume snapshot /btr_pool/update_snapshots/${SNAPSHOT} /btr_pool/@
+btrfs subvolume delete /btr_pool/@last
+# SNAPSHOT=@upd.$(date +"%Y%m%dT%H%M%S")
+# btrfs subvolume snapshot -r /btr_pool/@ /btr_pool/update_snapshots/${SNAPSHOT}
+mv /btr_pool/@ /btr_pool/@last
+# btrfs subvolume snapshot /btr_pool/update_snapshots/${SNAPSHOT} /btr_pool/@
+mv /btr_pool/@rw /btr_pool/@
 mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/@ /mnt
-mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/@root_user /mnt/root
+# mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/@root_user /mnt/root
+arch-chroot /mnt bash -c 'mount -a; umount /etc'
 arch-chroot /mnt
