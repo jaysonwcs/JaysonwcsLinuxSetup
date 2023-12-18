862f4d458263b11fd5ba4561f347a7752a1d1320
Jayson <jaysonwcs@gmail.com>
2023-12-18 15:22:45 -0300

    Updated finish-system-update.sh script

--- immutable-system/finish-system-update.sh
+++ immutable-system/finish-system-update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 umount -R /mnt
-grub-mkconfig -o /boot/grub/grub.cfg
+# grub-mkconfig -o /boot/grub/grub.cfg
+mv /btr_pool/@ /btr_pool/@rw
+btrfs subvolume snapshot -r /btr_pool/@rw /btr_pool/@
