cefb11c40a1ebb98157118c1497c64b02ed72087
Jayson <jaysonwcs@gmail.com>
2023-12-18 17:27:14 -0300

    chroot-download-repo.sh changed to download .dotfiles to root folder

--- arch-linux-installation/chroot-download-repo.sh
+++ arch-linux-installation/chroot-download-repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-arch-chroot $1 /bin/bash -c "su - -c 'cd ~/; git clone https://github.com/jaysonwcs/JaysonwcsLinuxSetup'"
+arch-chroot $1 /bin/bash -c "su - -c 'cd /; git clone https://github.com/jaysonwcs/JaysonwcsLinuxSetup'"
