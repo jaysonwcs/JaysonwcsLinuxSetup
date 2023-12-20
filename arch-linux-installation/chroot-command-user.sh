a3a3303d001558779b97ed7f112ac81b7853449a
Jayson <jaysonwcs@gmail.com>
2023-12-20 13:07:28 -0300

    fixed chroot-command-user.sh script

--- arch-linux-installation/chroot-command-user.sh
+++ arch-linux-installation/chroot-command-user.sh
@@ -2,4 +2,11 @@
 
 #./chroot-command-user.sh /mnt/system jayson '/' 'JaysonwcsLinuxSetup/download-setup-scripts.sh'
 
-arch-chroot $1 /bin/bash -c "su $2 - -c \"cd "$3"; "$4"\""
+echo "arch-chroot into \"$1\" directory"
+echo "Inside CHROOT:"
+echo "change to user \"$2\""
+echo "change to directory \"$3\""
+echo "execute command \"$4\""
+echo
+
+arch-chroot "$1" /bin/bash -c "su $2 - -c 'cd $3; $4'"
