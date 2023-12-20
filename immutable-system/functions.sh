189c0da03b2bbdc35e88965147ada60eff8180f7
Jayson <jaysonwcs@gmail.com>
2023-12-20 14:21:34 -0300

    fixed functions.sh

--- immutable-system/functions.sh
+++ immutable-system/functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 execute-in-update-system() {
-	~/jaysonwcsLinuxSetup/immutable-system/enter-update-system.sh
+	~/JaysonwcsLinuxSetup/immutable-system/enter-update-system.sh
 
 	/bin/arch-chroot "$@"
 
-	~/jaysonwcsLinuxSetup/immutable-system/finish-system-update.sh
+	~/JaysonwcsLinuxSetup/immutable-system/finish-system-update.sh
 }
