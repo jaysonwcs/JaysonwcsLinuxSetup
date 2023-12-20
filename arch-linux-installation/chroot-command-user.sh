395667fc804878d622e86e15b2436849c958591f
Jayson <jaysonwcs@gmail.com>
2023-12-20 09:54:06 -0300

    chroot-command-user.sh script changed to be more flexible and accept start folder as parameter

--- arch-linux-installation/chroot-command-user.sh
+++ arch-linux-installation/chroot-command-user.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-arch-chroot $1 /bin/bash -c "su $2 - -c 'cd ~; $3'"
+#./chroot-command-user.sh /mnt/system jayson '/' 'JaysonwcsLinuxSetup/download-setup-scripts.sh'
+
+arch-chroot $1 /bin/bash -c "su $2 - -c 'cd $3; $4'"
