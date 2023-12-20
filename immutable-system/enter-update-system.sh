19c47c4f964158f496a2fb7533e74a3706a6e0f1
Jayson <jaysonwcs@gmail.com>
2023-12-20 14:31:21 -0300

    trying new approach to immutable system scripts

--- immutable-system/enter-update-system.sh
+++ immutable-system/enter-update-system.sh
@@ -2,23 +2,18 @@
 
 #./enter-update-system.sh /dev/sda4
 
-# source functions.sh
+source functions.sh
 
-# btrfs subvolume delete /btr_pool/@last
-# # SNAPSHOT=@upd.$(date +"%Y%m%dT%H%M%S")
-# # btrfs subvolume snapshot -r /btr_pool/@ /btr_pool/update_snapshots/${SNAPSHOT}
-# mv /btr_pool/@ /btr_pool/@last
-# # btrfs subvolume snapshot /btr_pool/update_snapshots/${SNAPSHOT} /btr_pool/@
-# mv /btr_pool/@rw /btr_pool/@
+btrbk -S --progress run
 
-sudo btrbk -S --progress run
+mount "$1" -o noatime,compress=zstd,commit=120,subvol=/@rw /mnt
 
-sudo mount "$1" -o noatime,compress=zstd,commit=120,subvol=/@rw /mnt
-# mount /dev/sda4 -o noatime,compress=zstd,commit=120,subvol=/@root_user /mnt/root
-sudo /bin/arch-chroot /mnt bash << "_EOT_"
+/bin/arch-chroot /mnt bash << "_EOT_"
 mount -a
 umount /etc
 _EOT_
+
+execute-in-update-system /mnt
 
 # /bin/arch-chroot /mnt
 # arch-chroot /mnt zsh -c "su jayson - -c 'cd ~; $3'"
--- immutable-system/functions.sh
+++ immutable-system/functions.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 execute-in-update-system() {
-	sudo ~/JaysonwcsLinuxSetup/immutable-system/enter-update-system.sh
+	/bin/arch-chroot "$@"
 
-	sudo /bin/arch-chroot "$@"
-
-	sudo ~/JaysonwcsLinuxSetup/immutable-system/finish-system-update.sh
+	~/JaysonwcsLinuxSetup/immutable-system/finish-system-update.sh
 }
