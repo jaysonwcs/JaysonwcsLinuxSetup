f923ccdf0e602a7c18e061c68c2e6b7aab2b25e9
Jayson <jaysonwcs@gmail.com>
2023-12-20 10:01:35 -0300

    fixed chroot-command-user.sh script

--- arch-linux-installation/chroot-command-user.sh
+++ arch-linux-installation/chroot-command-user.sh
@@ -2,4 +2,4 @@
 
 #./chroot-command-user.sh /mnt/system jayson '/' 'JaysonwcsLinuxSetup/download-setup-scripts.sh'
 
-arch-chroot $1 /bin/bash -c "su $2 - -c 'cd "$3"; "$4"'"
+arch-chroot $1 /bin/bash -c "su $2 - -c \"cd "$3"; "$4"\""
